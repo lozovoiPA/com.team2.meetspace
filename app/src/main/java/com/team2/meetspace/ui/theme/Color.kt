802178b89c8a7c7fb3fa77356104856b521e83c9
Лозовой П.А. <pa.lozovoi@mail.ru>
2026-02-27 16:36:30 +0700

    Test feature commit

--- app/src/main/java/com/team2/meetspace/ui/theme/Color.kt
+++ app/src/main/java/com/team2/meetspace/ui/theme/Color.kt
@@ -10,4 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val CallBackground = Color(0xFF0C090E)
+val CallBackgroundColor = Color(0xFF0C090E)
+val ButtonColor = Color(0xFF884498)
